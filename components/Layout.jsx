13e911e241f74bd20d872d9cc2e9f0e574dd3bed
Mohammad Mehadi Hasan <mohhas@st.amu.edu.pl>
2023-05-13 02:54:52 +0200

    initial setup done

---
+++ components/Layout.jsx
@@ -0,0 +1,13 @@
+import Navigation from "./Sidebar";
+
+export default function Layout({children}){
+    return (
+        <div className="bg-slate-600 min-h-screen min-w-screen flex">
+          <Navigation />
+          <div className="bg-white flex-grow rounded-md p-2 m-2">
+          {children}
+          </div>
+          
+        </div>
+      )
+}
---
+++ components/Sidebar.jsx
@@ -0,0 +1,25 @@
+"use client"
+import Link from "next/link";
+//import { useRouter } from "next/router";
+import { useRouter } from 'next/navigation';
+
+export default function Navigation(){
+    const inactiveNav = "flex mt-2 gap-1 p-1";
+    const activeNav = "flex mt-2 gap-1 p-1 bg-gray-50 text-cyan-700 rounded-l-md";
+    const router = useRouter();
+    return(
+        <aside className="bg-slate-600 text-white p-2">
+        <nav className="flex flex-col gap-1">
+        
+        <Link href={'/'}>Link</Link>
+        <Link href={'/'}>Link</Link>
+        <Link href={'/'}>Link</Link>
+        <Link href={'/'}>Link</Link>
+        <Link href={'/'}>Link</Link>
+        <Link href={'/'} className={router.pathname === '/' ? activeNav : inactiveNav}>Linkdbdf</Link>
+
+
+        </nav>
+        </aside>
+    );
+}
