ace3cac2654011aa23f328a327d4edbc8525857b
Mohammad Mehadi Hasan <mohhas@st.amu.edu.pl>
2023-05-16 00:08:00 +0200

    Every Route is done

--- components/GridItems.jsx
+++ components/GridItems.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 export default function GridItems() {
     return(
+      <>
         <div className="mb-16 grid gap-2 text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
 
         <Link 
@@ -69,56 +70,86 @@ export default function GridItems() {
         </Link>
 
         <Link 
-          href={'/testiRt'}
+          href={'/dynamicProgramming'}
           className="gridItemsEach"
           rel="noopener noreferrer"
         >
         <h2 className={`mb-3 text-2xl font-semibold`}>
-            Basic Problems{' '}
+            Dynamic Programming{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Example Basic Problems
+            Example of Dynamic Programming
           </p>
         </Link>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
         
+        <Link 
+          href={'/recursion'}
+          className="gridItemsEach"
+          rel="noopener noreferrer"
+        >
+        <h2 className={`mb-3 text-2xl font-semibold`}>
+            Recursion{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Example of Recursion 
+          </p>
+        </Link>
+
+        <Link 
+          href={'/searching'}
+          className="gridItemsEach"
+          rel="noopener noreferrer"
+        >
+        <h2 className={`mb-3 text-2xl font-semibold`}>
+            Searching{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Example of Searching Algorithms 
+          </p>
+        </Link>
+
+        <Link 
+          href={'/sorting'}
+          className="gridItemsEach"
+          rel="noopener noreferrer"
+        >
+        <h2 className={`mb-3 text-2xl font-semibold`}>
+            Sorting Algorithms{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Example of Every Sorting Techniques 
+          </p>
+        </Link>
 
       </div>
+      <div className="relative top-2/3">
+      
+      <div className="group rounded-lg border border-transparent absolute inset-x-5 bottom-0 px-2 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100">
+      <h2 className={`mb-3 text-2xl font-medium`}>This Website was made using the latest features of NextJS</h2>
+      <Link
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Find in-depth information about Next.js features and API.</p>
+        </span>
+      </Link>
+      </div>
+      </div>
+
+      </>
     )
 }
